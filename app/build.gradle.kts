5131d81cb7cc31c5aa067f8651ec53ed18326572
Ben Piatt <piattb4@gmail.com>
2024-12-17 16:59:49 -0600

    Updated compile and target SDK to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.fetch.rewards"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.fetch.rewards"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
